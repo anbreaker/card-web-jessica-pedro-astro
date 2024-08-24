5e529b28ff1880fb1f31d1296fdb8f894b036e8d
anbreaker <antunez19@gmail.com>
2024-08-24 16:29:51 +0200

    feat: links for prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://anbreaker.github.io/card-web-jessica-pedro-astro/',
-  base: '/',
+  base: '/card-web-jessica-pedro-astro/',
   i18n: {
     defaultLocale: 'pt',
     locales: ['pt', 'es'],
