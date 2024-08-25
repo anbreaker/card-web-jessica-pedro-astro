d3060d73f9a81e506c6f0fbbfdc065e2d544df1f
anbreaker <antunez19@gmail.com>
2024-08-25 12:13:02 +0200

    feat: Update URLs for site and base in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://anbreaker.github.io/card-web-jessica-pedro-astro/',
-  base: '/card-web-jessica-pedro-astro/',
+  site: 'https://anbreaker.github.io/jessica-ns-pedro/',
+  base: '/jessica-ns-pedro/',
   i18n: {
     defaultLocale: 'pt',
     locales: ['pt', 'es'],
